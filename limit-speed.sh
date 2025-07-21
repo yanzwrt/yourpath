2031f942d7478ca652083013a5fd407e07276524
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-21 18:48:28 +0700

    limit-speed.sh

--- limit-speed.sh
+++ limit-speed.sh
@@ -3,7 +3,7 @@
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
 # Auther  : RakhaVPN
-# (C) Copyright 2022
+# (C) Copyright 2025
 # =========================================
 red='\e[1;31m'
 green='\e[0;32m'
@@ -15,13 +15,13 @@ cek=$(cat /home/limit)
 NIC=$(ip -o $ANU -4 route show to default | awk '{print $5}');
 function start () {
 clear
-echo -e "Limit Speed All Service"
-read -p "Set maximum download rate (in Kbps): " down
-read -p "Set maximum upload rate (in Kbps): " up
+echo -e "Batasi Kecepatan Semua Layanan"
+read -p "Tetapkan kecepatan pengunduhan maksimum (in Kbps): " down
+read -p "Tetapkan kecepatan Pengunggahan maksimum (in Kbps): " up
 if [[ -z "$down" ]] && [[ -z "$up" ]]; then
 echo > /dev/null 2>&1
 else
-echo "Start Configuration"
+echo "Mulai Konfigurasi"
 sleep 0.5
 wondershaper -a $NIC -d $down -u $up > /dev/null 2>&1
 systemctl enable --now wondershaper.service
@@ -36,7 +36,7 @@ function stop () {
 clear
 wondershaper -ca $NIC
 systemctl stop wondershaper.service
-echo "Stop Configuration"
+echo "Hentikan Konfigurasi"
 sleep 0.5
 echo > /home/limit
 echo "Done"
@@ -51,20 +51,20 @@ sts="${Error}"
 fi
 clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;47;30m            LIMIT BANDWITH SPEED            \E[0m"
+echo -e " \E[0;47;30m      BATASI KECEPATAN BANDWITH         \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m
 \033[1;37mLimit Bandwith Speed By RakhaVPN\033[0m
-\033[1;37mTelegram : https://t.me/todfix667 / @todfix667\033[0m"
+\033[1;37mTelegram : https://t.me/RakhaVPN / @RakhaVPN\033[0m"
 echo ""
 echo -e "   Status : $sts"
 echo -e "
- [\033[1;36m•1 \033[0m]  Start Limit
- [\033[1;36m•2 \033[0m]  Stop Limit
- [\033[1;36m•3 \033[0m]  Back To Main Menu"
+ [\033[1;36m•1 \033[0m]  Mulai Batas
+ [\033[1;36m•2 \033[0m]  Berhenti Batas
+ [\033[1;36m•3 \033[0m]  kembali ke menu"
 echo ""
-echo -e " \033[1;37mPress [ Ctrl+C ] • To-Exit-Script\033[0m"
+echo -e " \033[1;37mTekan [ Ctrl+C ] • Untuk-Keluar-Script\033[0m"
 echo ""
-read -rp "Select menu : " -e num
+read -rp "Pilih menu : " -e num
 if [[ "$num" = "1" ]]; then
 start
 elif [[ "$num" = "2" ]]; then
@@ -73,7 +73,7 @@ elif [[ "$num" = "3" ]]; then
 menu
 else
 clear
-echo " Please Enter The Correct Number!"
+echo " Silahkan Masukkan Nomor yang Benar!"
 sleep 0.5
 limit
 fi

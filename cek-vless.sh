d38abaf1b1625435c865c116d1a5569afeb2ea6a
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-21 18:16:49 +0700

    cek-vless.sh

--- cek-vless.sh
+++ cek-vless.sh
@@ -2,8 +2,8 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Auther  : NevermoreSSH
-# (C) Copyright 2022
+# Auther  : RakhaVPN
+# (C) Copyright 2025
 # =========================================
 clear
 red='\e[1;31m'
@@ -12,7 +12,7 @@ NC='\e[0m'
 echo -n > /tmp/other.txt
 data=( `cat /usr/local/etc/xray/vless.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \E[0;47;30m     XRAY Vless WS User Login      \E[0m"
+echo -e " \E[0;47;30m     Pengguna Login XRAY Vless WS      \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 for akun in "${data[@]}"
 do
@@ -46,5 +46,5 @@ rm -rf /tmp/other.txt
 done
 echo ""
 echo ""
-read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
+read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Kembali Ke Menu . . .") "
 menu

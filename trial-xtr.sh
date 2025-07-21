913686c24faea4590c1a80279d9779c5dbf309ea
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-21 19:35:47 +0700

    trial-xtr.sh

--- trial-xtr.sh
+++ trial-xtr.sh
@@ -2,8 +2,8 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition V1.0
-# Auther  : NevermoreSSH
-# (C) Copyright 2022
+# Auther  : RakhaVPN
+# (C) Copyright 2025
 # =========================================
 red='\e[1;31m'
 green='\e[0;32m'

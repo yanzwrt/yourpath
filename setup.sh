ed32c3ab65081ee826f9bee2176a4564598de3aa
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-22 00:06:53 +0700

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,88 @@
-echo -e "[ ${green}INFO${NC} ] Menyiapkan instalasi autoscript ~"
+#!/bin/bash
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V1.0
+# Auther  : yanzwrt
+# (C) Copyright 2025
+# =========================================
+clear
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+export Server_URL="raw.githubusercontent.com/yanzwrt/yourpath/main"
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+##########################
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP=$(curl -s ipinfo.io/ip )
+MYIP=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ifconfig.me )
+echo "Checking VPS"
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+purple='\e[0;35m'
+NC='\e[0m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+MYIP=$(wget -qO- icanhazip.com/ip);
+secs_to_human() {
+    echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minutes $(( ${1} % 60 )) seconds"
+}
+start=$(date +%s)
+
+echo -e "[ ${green}INFO${NC} ] Preparing the autoscript installation ~"
 apt install git curl -y >/dev/null 2>&1
-echo -e "[ ${green}INFO${NC} ] File instalasi siap untuk dipasang!"
+echo -e "[ ${green}INFO${NC} ] Installation file is ready to begin !"
 sleep 1
 
 if [ -f "/usr/local/etc/xray/domain" ]; then
-echo "Script sudah terpasang!"
+echo "Script Already Installed"
 exit 0
 fi
 
 mkdir /var/lib/premium-script;
 mkdir /var/lib/crot-script;
 clear
-
-echo -e "${red}    ♦️${NC} ${green} PENGATURAN DOMAIN VPS     ${NC}"
+#echo -e "${red}♦️${NC} ${green}Established By yanzwrt 2025${NC} ${red}♦️${NC}"
+#DOWNLOAD SOURCE SCRIPT
+echo -e "${red}    ♦️${NC} ${green} CUSTOM SETUP DOMAIN VPS     ${NC}"
 echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo "1. Gunakan domain otomatis dari script"
-echo "2. Masukkan domain sendiri"
+echo "1. Gunakan Domain Dari Script"
+echo "2. Pilih Domain Sendiri"
 echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-read -rp "Pilih metode pengaturan domain : " dom 
+read -rp "Choose Your Domain Installation : " dom 
 
 if test $dom -eq 1; then
 clear
@@ -25,65 +90,65 @@ wget -q -O /root/cf.sh "https://${Server_URL}/cf.sh"
 chmod +x /root/cf.sh
 ./cf.sh
 elif test $dom -eq 2; then
-read -rp "Masukkan nama domain anda : " domen 
+read -rp "Enter Your Domain : " domen 
 echo $domen > /root/domain
 else 
-echo "Pilihan tidak ditemukan!"
+echo "Not Found Argument"
 exit 1
 fi
-
-echo -e "${GREEN}Berhasil mengatur domain!${NC}"
+echo -e "${GREEN}Done!${NC}"
 sleep 2
 clear
-
 echo "IP=$host" >> /var/lib/premium-script/ipvps.conf
 echo "IP=$host" >> /var/lib/crot-script/ipvps.conf
 echo "$host" >> /root/domain
-
-# Instalasi SSH-VPN
-echo -e "\e[0;32mMEMULAI INSTALASI SSH-VPN...\e[0m"
+#clear
+#echo -e "\e[0;32mREADY FOR INSTALLATION SCRIPT...\e[0m"
+#echo -e ""
+#sleep 1
+#Install SSH-VPN
+echo -e "\e[0;32mINSTALLING SSH-VPN...\e[0m"
 sleep 1
 wget https://${Server_URL}/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 sleep 3
-
-# Instalasi XRAY Core
 clear
-echo -e "\e[0;32mMEMULAI INSTALASI XRAY CORE...\e[0m"
+echo -e "\e[0;32mINSTALLING XRAY CORE...\e[0m"
 sleep 3
 wget -q -O /root/xray.sh "https://${Server_URL}/xray.sh"
 chmod +x /root/xray.sh
 ./xray.sh
-echo -e "${GREEN}Selesai!${NC}"
+echo -e "${GREEN}Done!${NC}"
 sleep 2
-
-# Instalasi SET-BR
 clear
-echo -e "\e[0;32mMEMULAI INSTALASI SET-BR...\e[0m"
+#Install SET-BR
+echo -e "\e[0;32mINSTALLING SET-BR...\e[0m"
 sleep 1
 wget -q -O /root/set-br.sh "https://${Server_URL}/set-br.sh"
 chmod +x /root/set-br.sh
 ./set-br.sh
-echo -e "${GREEN}Selesai!${NC}"
+echo -e "${GREEN}Done!${NC}"
 sleep 2
 clear
 
-# Hapus sisa file instalasi
+#rm -rf /usr/share/nginx/html/index.html
+#wget -q -O /usr/share/nginx/html/index.html "https://raw.githubusercontent.com/yanzwrt/yourpath/main/OTHERS/index.html"
+
+# Finish
 rm -f /root/ins-xray.sh
 rm -f /root/set-br.sh
 rm -f /root/ssh-vpn.sh
 
-# Versi
+# Version
 echo "1.0" > /home/ver
-
 clear
 echo ""
 echo -e "${RB}      .-------------------------------------------.${NC}"
-echo -e "${RB}      |${NC}     ${CB}INSTALASI TELAH SELESAI${NC}             ${RB}|${NC}"
+echo -e "${RB}      |${NC}      ${CB}Installation Has Been Completed${NC}      ${RB}|${NC}"
 echo -e "${RB}      '-------------------------------------------'${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "     ${WB}Multiport Websocket Autoscript Oleh Rakha-VPN${NC}"
+echo -e "      ${WB}Multiport Websocket Autoscript By yanzwrt${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "  ${WB}»»» Layanan Protokol «««          |  »»» Jaringan Protokol «««${NC}  "
+echo -e "  ${WB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}Vmess Websocket${NC}         ${WB}|${NC}  ${YB}- Websocket (CDN) TLS${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}Vless Websocket${NC}         ${WB}|${NC}  ${YB}- Websocket (CDN) NTLS${NC}"
@@ -91,7 +156,7 @@ echo -e "  ${RB}♦️${NC} ${YB}Trojan Websocket${NC}        ${WB}|${NC}  ${YB}
 echo -e "  ${RB}♦️${NC} ${YB}Trojan TCP XTLS${NC}         ${WB}|${NC}  ${YB}- TCP TLS${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}Trojan TCP${NC}              ${WB}|${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "          ${WB}»»» Informasi YAML Config «««${NC}            "
+echo -e "           ${WB}»»» YAML Service Information «««${NC}          "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}YAML XRAY VMESS WS${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}YAML XRAY VLESS WS${NC}"
@@ -99,33 +164,36 @@ echo -e "  ${RB}♦️${NC} ${YB}YAML XRAY TROJAN WS${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}YAML XRAY TROJAN XTLS${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}YAML XRAY TROJAN TCP${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "             ${WB}»»» Informasi Server «««${NC}               "
+echo -e "             ${WB}»»» Server Information «««${NC}                 "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Zona Waktu            : Asia/Kuala_Lumpur (GMT +8)${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Fail2Ban              : [AKTIF]${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Dflate                : [AKTIF]${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}IPtables              : [AKTIF]${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Auto-Reboot           : [AKTIF]${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}IPV6                  : [NONAKTIF]${NC}"
-echo ""
-echo -e "  ${RB}♦️${NC} ${YB}Auto reboot pukul 06.00 (GMT +8)${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Backup & Restore Data VPS${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Hapus Otomatis Akun Expired${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Pantau Bandwidth${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Pantau RAM & CPU${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Cek Login Pengguna${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Cek Config Dibuat${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Clear Log Otomatis${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Timezone                : Asia/Kuala_Lumpur (GMT +8)${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Fail2Ban                : [ON]${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Dflate                  : [ON]${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}IPtables                : [ON]${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Auto-Reboot             : [ON]${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}IPV6                    : [OFF]${NC}"
+echo -e ""
+echo -e "  ${RB}♦️${NC} ${YB}Autoreboot On 06.00 GMT +8${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Backup & Restore VPS Data${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Automatic Delete Expired Account${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Bandwith Monitor${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}RAM & CPU Monitor${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Check Login User${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Check Created Config${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}Automatic Clear Log${NC}"
 echo -e "  ${RB}♦️${NC} ${YB}Media Checker${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}Pengganti DNS${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}DNS Changer${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "           ${WB}»»» Informasi Port Jaringan «««${NC}          "
+echo -e "              ${WB}»»» Network Port Service «««${NC}             "
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}HTTP    : 80, 8080, 8880${NC}"
-echo -e "  ${RB}♦️${NC} ${YB}HTTPS   : 443${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}HTTP                    : 80, 8080, 8880${NC}"
+echo -e "  ${RB}♦️${NC} ${YB}HTTPS                   : 443${NC}"
 echo -e "${BB}————————————————————————————————————————————————————————${NC}"
 echo ""
 secs_to_human "$(($(date +%s) - ${start}))"
 echo ""
-read -p "$( echo -e "Tekan ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} untuk Reboot") "
+rm -r setup.sh
+echo ""
+echo ""
+read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} For Reboot") "
 reboot

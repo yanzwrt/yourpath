dfcc83384b8a73d1b74dce0f59e041b246d3ab62
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-21 19:09:59 +0700

    restart.sh

--- restart.sh
+++ restart.sh
@@ -21,7 +21,7 @@ fun_bar() {
         touch $HOME/fim
     ) >/dev/null 2>&1 &
     tput civis
-    echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+    echo -ne "  \033[0;33mMohon Tunggu Memuat \033[1;37m- \033[0;33m["
     while true; do
         for ((i = 0; i < 18; i++)); do
             echo -ne "\033[0;32m#"
@@ -32,9 +32,9 @@ fun_bar() {
         sleep 1s
         tput cuu1
         tput dl1
-        echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
+        echo -ne "  \033[0;33mMohon Tunggu Memuat \033[1;37m- \033[0;33m["
     done
-    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m BERHASIL!\033[1;37m"
     tput cnorm
 }
 res1() {
@@ -73,33 +73,33 @@ res11() {
 
 clear
 echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
-echo -e " \E[0;41;36m           RESTART SERVICE SERVER           \E[0m"
+echo -e " \E[0;41;36m           RESTART LAYANAN SERVER           \E[0m"
 echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
 echo -e ""
-echo -e "  \033[1;91m Service Restart Fail2ban\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Fail2ban\033[1;37m"
 fun_bar 'res1'
-echo -e "  \033[1;91m Service Restart Cron\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Cron\033[1;37m"
 fun_bar 'res2'
-echo -e "  \033[1;91m Service Restart Nginx\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Nginx\033[1;37m"
 fun_bar 'res3'
-echo -e "  \033[1;91m Service Restart Vmess TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Vmess TLS\033[1;37m"
 fun_bar 'res4'
-echo -e "  \033[1;91m Service Restart Vmess None TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Vmess Tanpa TLS\033[1;37m"
 fun_bar 'res5'
-echo -e "  \033[1;91m Service Restart Vless TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Vless TLS\033[1;37m"
 fun_bar 'res6'
-echo -e "  \033[1;91m Service Restart Vless None TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Vless Tanpa TLS\033[1;37m"
 fun_bar 'res7'
-echo -e "  \033[1;91m Service Restart Trojan TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Trojan WS\033[1;37m"
 fun_bar 'res8'
-echo -e "  \033[1;91m Service Restart Trojan None TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Trojan Tanpa TLS\033[1;37m"
 fun_bar 'res9'
-echo -e "  \033[1;91m Service Restart Trojan TCP XTLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Trojan TCP XTLS\033[1;37m"
 fun_bar 'res10'
-echo -e "  \033[1;91m Service Restart Trojan TCP TLS\033[1;37m"
+echo -e "  \033[1;91m Memulai ulang layanan Trojan TCP TLS\033[1;37m"
 fun_bar 'res11'
 echo -e ""
-echo -e "Script Mod By RakhaVPN"
+echo -e "Script Mod Oleh RakhaVPN"
 echo ""
-read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} Back to menu . . .") "
+read -p "$( echo -e "Tekan ${orange}[ ${NC}${green}Enter${NC} ${orange}]${NC} untuk kembali ke menu . . .") "
 menu

e2bee3c0b1dfc81ec80b216b012a2dcf571777e6
yanzwrt <165026256+yanzwrt@users.noreply.github.com>
2025-07-23 10:32:49 +0700

    status.sh

--- status.sh
+++ status.sh
@@ -14,96 +14,96 @@ NC='\e[0m'
 
 clear
 echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-echo -e " ${green}            STATUS LAYANAN SERVER VPS            ${NC}"
+echo -e " ${green}     🖥️  STATUS LAYANAN SERVER VPS  🖥️        ${NC}"
 echo -e "${red}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo ""
 
 status="$(systemctl show cron.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} Cron Service               : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} Cron Service               : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} Cron Service               : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} Cron Service               : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show nginx.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} Nginx Web Server           : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} Nginx Web Server           : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} Nginx Web Server           : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} Nginx Web Server           : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show fail2ban.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} Fail2Ban Security          : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} Fail2Ban Security          : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} Fail2Ban Security          : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} Fail2Ban Security          : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Vmess TLS             : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vmess TLS             : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Vmess TLS             : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vmess TLS             : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@vless.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Vless TLS             : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vless TLS             : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Vless TLS             : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vless TLS             : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@trojanws.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Trojan TLS (WS)       : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan TLS (WS)       : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Trojan TLS (WS)       : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan TLS (WS)       : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@none.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Vmess Non TLS         : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vmess Non TLS         : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Vmess Non TLS         : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vmess Non TLS         : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@vnone.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Vless Non TLS         : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vless Non TLS         : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Vless Non TLS         : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Vless Non TLS         : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@trnone.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Trojan Non TLS        : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan Non TLS        : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Trojan Non TLS        : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan Non TLS        : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@xtrojan.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Trojan TCP XTLS       : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan TCP XTLS       : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Trojan TCP XTLS       : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan TCP XTLS       : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 status="$(systemctl show xray@trojan.service --no-page)"
 status_text=$(echo "${status}" | grep 'ActiveState=' | cut -f2 -d=)
 if [ "${status_text}" == "active" ]; then
-  echo -e " ${purple}♦${NC} XRAY Trojan TCP TLS        : ${green}Aktif (Berjalan)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan TCP TLS        : ${green}✅ Aktif (Berjalan)${NC}"
 else
-  echo -e " ${purple}♦${NC} XRAY Trojan TCP TLS        : ${red}Tidak Aktif (Error)${NC}"
+  echo -e " ${purple}♦${NC} XRAY Trojan TCP TLS        : ${red}❌ Tidak Aktif (Error)${NC}"
 fi
 
 echo ""
